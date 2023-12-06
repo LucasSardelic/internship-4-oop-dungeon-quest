bcd2cea39817b0389e513f4063e4d03492135805
LucasSardelic <sardelic.lucas@gmail.com>
2023-12-06 15:02:08 +0100

    Add project files.

---
+++ Library.Data/DungeonStats.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Data
+{
+    public class DungeonStats
+    {
+        public DungeonStats()
+        {
+            var DungeonMobs = new Dictionary<int, (string, int)>()
+            {
+                {1, ("goblin", 50)}
+            };
+        }
+    }
+}
